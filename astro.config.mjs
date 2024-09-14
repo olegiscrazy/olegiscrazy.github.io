da7031df2dcb7040dfb680bec874fcc5ddb4fa12
olegiscrazy <oleg.semikopenko@epitech.eu>
2024-09-14 22:16:10 +0200

    [ADD] added more stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://olegiscrazy.github.io',
-    integrations: [tailwind()]
+    integrations: [tailwind()],
+    server: {
+        port: 3000,
+    },
+    // astro preferences disable devToolbar
 });
